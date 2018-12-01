2e27fab23eb255f9274a8a22d4fd226ad5b350ee
Calvin Huang <calvin.peak@capslock.tw>
2018-12-01 18:59:27 +0800

    feat(locale): add change locale mechanism for root controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    # Default locale tw
+    cookies[:locale] = params[:locale] || (cookies[:locale] || 'zh-TW')
+    I18n.locale = cookies[:locale]
+  end
 end
